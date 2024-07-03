80d5ecf3fc9f5904eec24219f703326da35a820c
radeel2024 <radeelprojet@gmail.com>
2024-07-03 13:05:02 +0100

    fix bugs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,9 +27,9 @@
     <link href="css/style.css" rel="stylesheet">
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <style>
         .news,
@@ -133,6 +133,7 @@
             object-fit: cover;
         }
     </style>
+
 </head>
 
 <body>
