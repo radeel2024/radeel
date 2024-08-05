0bb6f33a0163a116d1fe0654147e3ad7cbca15aa
radeel2024 <radeelprojet@gmail.com>
2024-08-05 08:17:52 +0100

    fix bugs

--- app/Http/Middleware/CheckIp.php
+++ app/Http/Middleware/CheckIp.php
@@ -21,7 +21,7 @@ class CheckIp
         if (!in_array($request->ip(), $adress)) {
             return response('
                             <!DOCTYPE html>
-                            <html lang="en">
+                            <html>
                             <head>
                             <meta charset="utf-8">
                             <meta http-equiv="X-UA-Compatible" content="IE=edge">
